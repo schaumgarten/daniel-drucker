1c12f8d391b4d7b77e835e72b8934325626dba1b
Oscar Zoletto <oscar.zoletto@konfio.mx>
2020-07-17 13:56:59 +0300

    Updates

---
+++ precache-manifest.939777676dcb126d6c2520689ae075ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc8f9d0613395f0fb708634175ab78a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c703b6bdd00d0899da2a",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1754d1189b2c0e45f97d",
+    "url": "./static/js/2.d0d0b334.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.d0d0b334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c703b6bdd00d0899da2a",
+    "url": "./static/js/main.294124b2.chunk.js"
+  },
+  {
+    "revision": "d7d8c1e70a949d20a4e4",
+    "url": "./static/js/runtime-main.af2688d8.js"
+  },
+  {
+    "revision": "58eaa8282467a5175d5c86e3d0718d01",
+    "url": "./static/media/1363469042 copia.58eaa828.jpg"
+  },
+  {
+    "revision": "149f574a8699c09fa0df4b4c327dd752",
+    "url": "./static/media/ashley-picture-2.149f574a.jpg"
+  }
+]);
